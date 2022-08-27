78d90332f458d058deed860e9f9c853a433b9fd1
Star-Mech <HammadStarMech@gmail.com>
2022-08-27 19:11:33 +0500

    Bug Fixing
    Now the docstring is clear and some bug fixing

--- memoizer.py
+++ memoizer.py
@@ -1,18 +1,31 @@
 # Use memoization to save and use answer for repeating problems
-def memo_wrapper(**kwargs):  # Pass key_list=[list of arguments to be used in key]
-    """Attributes:
-    key_list: list of input args number to be used as memoizer"""
+def memo_wrapper(*ar, **kw):  # Pass key_list=[list of arguments to be used in key]
+    """ Function wraps recursive function and adds memoization.
+
+    Takes as argument, the argument numbers as well as kwargs to pinpoint the
+    decorated function arguments to be used for naming dict.
+
+    Attributes:
+    :key key_list (kwarg): A list containing arg numbers and kwarg name to be used as memoizer dict key.
+    if key not specified, then a key containing all args and kwargs is used for memo
+    """
 
     def memoizer(func):
         memo = {}
-        key_list = kwargs.get('key_list')
+        key_list = kw.get('key_list')
 
         def wrapper(*args, **kwargs):
-            key_memo = tuple(args[i] if type(i) is int else kwargs.get(i) for i in key_list)
+            if key_list:
+                key_memo = tuple(args[i] if type(i) is int else kwargs.get(i) for i in key_list)
+            else:
+                key_memo = args
             if args not in memo:
-                memo[args] = func(*args, **kwargs)
-            return memo[args]
+                memo[key_memo] = func(*args, **kwargs)
+            return memo[key_memo]
 
         return wrapper
 
-    return memoizer
+    if kw:
+        return memoizer
+    else:
+        return memoizer(*ar)
