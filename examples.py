a0edb7b44df60116a34797fce8ac00fedc4b5efe
Star-Mech <102207729+Star-Mech@users.noreply.github.com>
2022-08-27 19:10:14 +0500

    This file contains examples and usage
    Usage and examples of memoizer decorator shown in this file.

---
+++ examples.py
@@ -0,0 +1,33 @@
+from memoizer import memo_wrapper # import based on your directories
+
+
+
+# @memo_wrapper without arguments will also work
+@memo_wrapper(key_list=[0]) # fib(arg0=n)
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)  # Problems are repeated on both sides to add to a time complexity of O(n^2)
+
+# Without memoization, it will be 2^200 problems, with memoization it's only 200 problems
+print(fib(200))
+#25477612258980856902730428600
+
+
+# n = arg0, m = arg1
+# @memo_wrapper(key_list=[0,1]) # This acheives the same result
+@memo_wrapper
+def grid_traveler(n, m):# Without memo, time complexity = O(2^(m+n)), Space Complexity = O(m+n)
+    if n == 0 or m == 0:  #
+        return 0
+    # below case, neither is 0, but either is 1
+    elif n == 1 or m == 1:  # Only one way to travel in this case
+        return 1
+
+    return grid_traveler(n-1,m) + grid_traveler(n, m-1) # n-1 for down move, m-1 for right move
+
+# Without memoization, it will be 2^(80+80-4) problems, with memoization it's only 160 problems
+print(grid_traveler(80,80))
+#280571172992510140037611932413038677189525
